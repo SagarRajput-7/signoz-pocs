a779f60c2fb55eb9b46387176c4a3b176dd5423d
SagarRajput-7 <sagar@signoz.io>
2024-11-18 07:52:28 +0530

    deployment related changes

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "/Signoz-ui-pocs/",
+  build: {
+    outDir: "build", // Change output directory to 'build'
+  },
 });
